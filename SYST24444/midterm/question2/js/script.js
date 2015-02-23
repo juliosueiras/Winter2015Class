b5e003fce1c8ad8a635d7c7728b4ec7ffb76932b
Julio Tain Sueiras <juliosueiras@gmail.com>
2015-02-23 13:30:01 -0500

    Automated Crontab:Mon Feb 23 13:30:01 EST 2015

--- SYST24444/midterm/question2/js/script.js
+++ SYST24444/midterm/question2/js/script.js
@@ -12,20 +12,3 @@ function switchColor(targetItem) {
 
 }
 
-
-$("h1").on("click", switchColor("h1"));
-
-
-$(".flowerImage").focusin( function () {
-    var width = $(this).css("width");
-    var height = $(this).css("height");
-    $(this).css("width", width*200 );
-    $(this).css("height", height*200 );
-});
-
-$(".flowerImage").focusout( function () {
-    var width = $(this).css("width");
-    var height = $(this).css("height");
-    $(this).css("width", width/200 );
-    $(this).css("height", height/200 );
-});
