17b39bdb99216b7f10e11efab9b0c4a14fa7e1df
Julio Tain Sueiras <juliosueiras@gmail.com>
2015-02-23 14:20:01 -0500

    Automated Crontab:Mon Feb 23 14:20:01 EST 2015

--- SYST24444/midterm/question1/js/script.js
+++ SYST24444/midterm/question1/js/script.js
@@ -15,30 +15,47 @@
 
             if(xmlData)
             {
+
+                var userMake = document.getElementsByName("make").value;
+                var userModel = document.getElementsByName("make").value;
+                var itExist;
+                var userSelectedCar;
+
                 xmlData=(new DOMParser()).parseFromString(xml.responseText, 'text/xml');
                 var carInfo=xmlData.getElementsByTagName("carInfo");
 
+
                 for (var i=0; i < carInfo.length; ++i) {
+
+
                     var car=carInfo[i].getElementsByTagName("car")[0].firstChild.data;
                     var make=carInfo[i].getElementsByTagName("make")[0].getElementsByTagName("lname")[0].firstChild.data;
+
+                    if(make===userMake && model===userModel){
+                        itExist = true;
+                        userSelectedCar = carInfo[i];
+                        break;
+                    }
+
                     var model=carInfo[i].getElementsByTagName("model")[0].getElementsByTagName("month")[0].firstChild.data;
                 }
 
-                for(var i=0; i<carInfo.length; i++)
-                {
-                    var car=carInfo[i].getElementsByTagName("car")[0].firstChild.data;
-                    var serialNumber=carInfo[i].getElementsByTagName("serialNumber")[0].getElementsByTagName("fname")[0].firstChild.data;
-                    var make=carInfo[i].getElementsByTagName("make")[0].getElementsByTagName("lname")[0].firstChild.data;
+                if(itExist){
+                    var car=userSelectedCar.getElementsByTagName("car")[0].firstChild.data;
+                    var serialNumber=userSelectedCar.getElementsByTagName("serialNumber")[0].getElementsByTagName("fname")[0].firstChild.data;
+                    var make=userSelectedCar.getElementsByTagName("make")[0].getElementsByTagName("lname")[0].firstChild.data;
 
-                    var model=carInfo[i].getElementsByTagName("model")[0].getElementsByTagName("month")[0].firstChild.data;
-                    var kilometer=carInfo[i].getElementsByTagName("kilometer")[0].getElementsByTagName("day")[0].firstChild.data;
-                    var price=carInfo[i].getElementsByTagName("price")[0].getElementsByTagName("year")[0].firstChild.data;
+                    var model=userSelectedCar.getElementsByTagName("model")[0].getElementsByTagName("month")[0].firstChild.data;
+                    var kilometer=userSelectedCar.getElementsByTagName("kilometer")[0].getElementsByTagName("day")[0].firstChild.data;
+                    var price=userSelectedCar.getElementsByTagName("price")[0].getElementsByTagName("year")[0].firstChild.data;
 
                     document.write("<td>"+serialNumber+"</td>");
                     document.write("<td>"+make+"</td>");
                     document.write("<td>"+model+"</td>");
                     document.write("<td>"+kilometer+"</td>");
                     document.write("<td>"+price+"</td>");
+                }else{
+                    document.write("Sorry, we don’t have this car in store!");
                 }
             }
         }
