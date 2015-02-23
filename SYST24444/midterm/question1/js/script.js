855fa9afdd2d34a36f0e19aa99352cea9ed116b5
Julio Tain Sueiras <juliosueiras@gmail.com>
2015-02-23 14:10:01 -0500

    Automated Crontab:Mon Feb 23 14:10:01 EST 2015

--- SYST24444/midterm/question1/js/script.js
+++ SYST24444/midterm/question1/js/script.js
@@ -1,39 +1,44 @@
-        function readXML()
+        function searchCar()
         {
             var xml=new XMLHttpRequest();
-            xml.open('GET','employee.xml',false);
+            xml.open('GET','../xml/autogen.xml',false);
             xml.send();
             var xmlData=xml.response;
 
+
+
             document.write('<table border="1">');
-            document.write('<tr><th rowspan="2">ID</th><th colspan="2">Name</th>'+
-                           '<th colspan="3">Date of Birth</th><th rowspan="2">Department</th>');
-            document.write('<tr><th>First name</th><th>Last name</th>' +
+            document.write('<tr><th rowspan="2">Serial Number</th><th colspan="2">Make</th>'+
+                           '<th colspan="3">Model</th><th rowspan="2">Kilometer</th>');
+            document.write('<tr><th>Price</th><th>Last name</th>' +
                            '<th>Month</th><th>Day</th><th>Year</th></tr>');
 
             if(xmlData)
             {
                 xmlData=(new DOMParser()).parseFromString(xml.responseText, 'text/xml');
-                var emp=xmlData.getElementsByTagName("employee");
+                var carInfo=xmlData.getElementsByTagName("carInfo");
 
-                for(var i=0; i<emp.length; i++)
+                for (var i=0; i < carInfo.length; ++i) {
+                    var car=carInfo[i].getElementsByTagName("car")[0].firstChild.data;
+                    var make=carInfo[i].getElementsByTagName("make")[0].getElementsByTagName("lname")[0].firstChild.data;
+                    var model=carInfo[i].getElementsByTagName("model")[0].getElementsByTagName("month")[0].firstChild.data;
+                }
+
+                for(var i=0; i<carInfo.length; i++)
                 {
-                    var id=emp[i].getElementsByTagName("id")[0].firstChild.data;
-                    var fname=emp[i].getElementsByTagName("name")[0].getElementsByTagName("fname")[0].firstChild.data;
-                    var lname=emp[i].getElementsByTagName("name")[0].getElementsByTagName("lname")[0].firstChild.data;
+                    var car=carInfo[i].getElementsByTagName("car")[0].firstChild.data;
+                    var serialNumber=carInfo[i].getElementsByTagName("serialNumber")[0].getElementsByTagName("fname")[0].firstChild.data;
+                    var make=carInfo[i].getElementsByTagName("make")[0].getElementsByTagName("lname")[0].firstChild.data;
 
-                    var month=emp[i].getElementsByTagName("dob")[0].getElementsByTagName("month")[0].firstChild.data;
-                    var day=emp[i].getElementsByTagName("dob")[0].getElementsByTagName("day")[0].firstChild.data;
-                    var year=emp[i].getElementsByTagName("dob")[0].getElementsByTagName("year")[0].firstChild.data;
+                    var model=carInfo[i].getElementsByTagName("model")[0].getElementsByTagName("month")[0].firstChild.data;
+                    var kilometer=carInfo[i].getElementsByTagName("kilometer")[0].getElementsByTagName("day")[0].firstChild.data;
+                    var price=carInfo[i].getElementsByTagName("price")[0].getElementsByTagName("year")[0].firstChild.data;
 
-                    var dept=emp[i].getElementsByTagName("dept")[0].firstChild.data;
-                    document.write("<tr><td>"+id+"</td>");
-                    document.write("<td>"+fname+"</td>");
-                    document.write("<td>"+lname+"</td>");
-                    document.write("<td>"+month+"</td>");
-                    document.write("<td>"+day+"</td>");
-                    document.write("<td>"+year+"</td>");
-                    document.write("<td>"+dept+"</td><tr>");
+                    document.write("<td>"+serialNumber+"</td>");
+                    document.write("<td>"+make+"</td>");
+                    document.write("<td>"+model+"</td>");
+                    document.write("<td>"+kilometer+"</td>");
+                    document.write("<td>"+price+"</td>");
                 }
             }
         }
